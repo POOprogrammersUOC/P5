976648377cd05d1a9bba4fb3b794f9e45b7d8b0b
itutusausb <67843337+itutusausb@users.noreply.github.com>
2021-03-06 19:13:55 +0100

    Inicio Proyecto

---
+++ P5AA2/src/Ong.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Ong {
+
+	private String Nombre;
+	private String Direccion;
+	private String CIF;
+	private String Pais;
+	private String Telefono;
+	private String Web;
+	private String Email;
+	private ArrayList<Empleados> ListaEmpleados;
+	private ArrayList<Socios> ListaSocios;
+	private ArrayList<Proyectos> ListaProyectos;
+	
+	/**
+	 *  
+	 */
+	public Ong() {
+	}
+	
+	
+}
+ 
---
+++ P5AA2/src/Proyectos.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Proyectos {
+
+	private String Pais;
+	private String Localizacion;
+	private String lineaDeAccion;
+	private String SublineaDeAccion;
+	private Date FechaInicio;
+}
+
+ 
